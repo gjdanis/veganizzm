1201e3792f7d92cffed25f1b7301b75ccd110502
gjd <george.j.danis@gmail.com>
2016-04-09 16:11:36 -0400

    initial commit

---
+++ blog/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from recipes.models import Recipe
+
+class Post(models.Model):
+	"""
+	Represents a Veganizzm blog/recipe post
+
+	Attributes:
+		title (str): title to display of the post
+		recipe (Recipe|null): what the post is about (not all posts need have recipes)
+		content (str): the raw content of the post
+		date_created (date): when the post was created
+		published (bool): whether or not the post is live
+	"""
+
+	title = models.CharField(max_length=100)
+	recipe = models.OneToOneField(Recipe, null=True, on_delete=models.PROTECT)
+	content = models.TextField()
+	date_created = models.DateTimeField(auto_now_add=True)
+	published = models.BooleanField(default=True)
+
+	class Meta:
+		ordering = ["-date_created"]
+
+
---
+++ blog/tests.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from blog.models import Post
+from recipes.models import Recipe
+
+class PostTests(TestCase):
+	fixtures = ['sample-blog.json']
+
+	def test_delete_recipe(self):
+		pass
+
+	def test_delete_post(self):
+		pass
