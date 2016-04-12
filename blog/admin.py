01fd1306f5b9671a50f9ecfbd9baaade4717c004
gjd <george.j.danis@gmail.com>
2016-04-11 22:37:20 -0400

    Register models on the admin site

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from blog.models import Post
 
-# Register your models here.
+@admin.register(Post)
+class AdminPost(admin.ModelAdmin):
+    pass
--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from recipes.models import Ingredient, IngredientQuantity, RecipeStep, Recipe
 
-# Register your models here.
+@admin.register(Ingredient)
+class AdminIngredient(admin.ModelAdmin):
+    pass
+
+@admin.register(IngredientQuantity)
+class AdminIngredientQuantity(admin.ModelAdmin):
+    pass
+
+@admin.register(RecipeStep)
+class AdminRecipeStep(admin.ModelAdmin):
+    pass
+
+@admin.register(Recipe)
+class AdminRecipe(admin.ModelAdmin):
+    pass
