f18fc0b21f10bccfd7099097d465297626736b95
gjd <george.j.danis@gmail.com>
2016-04-16 00:42:51 -0400

    Add templates and common resource directories

--- veganizzm/settings.py
+++ veganizzm/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'veganizzm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(os.path.join(BASE_DIR, 'veganizzm'), 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,5 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 FIXTURE_DIRS = (os.path.join(os.path.join(BASE_DIR, 'veganizzm'), 'fixtures'),)
--- veganizzm/urls.py
+++ veganizzm/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+    url(r"^admin/", admin.site.urls),
+    url(r"^$", "blog.views.landing_view", name="landing_view"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
